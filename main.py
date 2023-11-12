58d8f6e2da17585e3ac0e52f046fed5bb735c902
r <p112000@gmail.com>
2023-11-12 13:53:34 -0800

    first working gui

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import filedialog
+from pytube import YouTube
+from tkinter import filedialog, messagebox
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
@@ -11,11 +12,16 @@ class YouTubeDownloaderGUI:
 
         # Variables
         self.output_path = tk.StringVar()
+        self.video_url = tk.StringVar()
 
         # Create GUI elements
         self.create_widgets()
 
     def create_widgets(self):
+        # Video URL Entry
+        tk.Label(self.root, text="Video URL:").pack(pady=10)
+        tk.Entry(self.root, textvariable=self.video_url, width=50).pack(pady=5)
+
         # Output Path Entry
         tk.Label(self.root, text="Output Path:").pack(pady=10)
         tk.Entry(self.root, textvariable=self.output_path, width=50, state="disabled").pack(pady=5)
@@ -29,16 +35,32 @@ class YouTubeDownloaderGUI:
         self.output_path.set(output_path)
 
     def download_video(self):
-        # Check if output path is selected
+        # Check if video URL and output path are selected
+        if not self.video_url.get():
+            messagebox.showerror("Error", "Please enter the video URL.")
+            return
         if not self.output_path.get():
-            tk.messagebox.showerror("Error", "Please select the output path.")
+            messagebox.showerror("Error", "Please select the output path.")
             return
 
         # Replace this part with your video download logic using the YouTube API
-        # Example: Display a message with the selected output path
-        tk.messagebox.showinfo("Info", f"Video will be downloaded to: {self.output_path.get()}")
+        yt = YouTube(self.video_url.get())
+        video = yt.streams.get_highest_resolution()
+
+        # download the video
+        video.download(self.output_path.get())
+        # Example: Display a message with the selected output path and video URL
+        messagebox.showinfo("Info", f"Downloading video from {self.video_url.get()} to {self.output_path.get()}")
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = YouTubeDownloaderGUI(root)
     root.mainloop()
+
+
+def download_video_from_youtube(link, path):
+    yt = YouTube(link)
+    video = yt.streams.get_highest_resolution()
+
+    # download the video
+    video.download(path)
